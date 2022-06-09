06c64f5d049ce9edaf89b8c8843d6035ab5364b9
pacharya92 <prakashacharya1992@gmail.com>
2022-06-09 01:13:36 -0400

    Add google_map gem to use the Google Maps API

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  Google::Maps.configure do |config|
+    config.authentication_mode = Google::Maps::Configuration::API_KEY
+    # Load via dotenv never commit API_KEY
+    config.api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  end
 end

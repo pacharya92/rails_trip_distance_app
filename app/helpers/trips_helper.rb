0dcc95370d0af77b99baca07be55761d94098f2f
pacharya92 <prakashacharya1992@gmail.com>
2022-06-18 23:56:36 -0400

    Move table string formatting methods to helper file
      - Remove trailing zeros from distance

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,12 @@
 module TripsHelper
+  # Convert seconds to hours and minutes
+  def seconds_to_str(seconds)
+    if seconds.present?
+      Time.at(seconds).utc.strftime("%kh %Mmin %Ss")
+      #"#{Time.at(seconds).utc.strftime("%k")}h #{Time.at(seconds).utc.strftime("%M")}min and #{Time.at(seconds).utc.strftime("%S")}s"
+    end
+  end
+  def address_format(location)
+    "#{location.street_number} #{location.street_name}, #{location.city}, #{location.governing_district} #{location.zip_code}, #{location.country}"
+  end
 end

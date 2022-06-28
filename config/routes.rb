8136426de0ac38667649dc5b044bc53faa96d4b9
pacharya92 <prakashacharya1992@gmail.com>
2022-06-28 00:09:57 -0400

    Add navbar on show_trip page

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'registrations#new'
   post '/sign_up', to: 'registrations#create'
 
-  delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
 
 end
